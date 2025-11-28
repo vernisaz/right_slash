e6380a83fdfc24d7e99a568bb9ddd27ccbfa2be3
D R <metricstream@gmail.com>
2025-11-27 21:35:58 -0800

    applied clippy suggestion

--- lib.rs
+++ lib.rs
@@ -4,8 +4,8 @@ pub fn to_unix_separator(mut path: String ) -> String {
     unsafe {
         let path_vec: &mut [u8]= path.as_bytes_mut();
     
-        for c in 0..path_vec.len() {
-            if path_vec[c] == b'\\' { path_vec[c] = b'/';}
+        for comp in path_vec {
+            if *comp == b'\\' { *comp = b'/';}
         }
     }
     path
@@ -14,8 +14,8 @@ pub fn to_unix_separator(mut path: String ) -> String {
 pub fn adjust_separator(mut path: String) -> String {
     let foreign_slash = if MAIN_SEPARATOR == '\\' { '/' } else { '\\' };
     let vec = unsafe {path.as_mut_vec()};
-    for c in 0..vec.len() {
-        if vec[c] == foreign_slash as u8 { vec[c] = MAIN_SEPARATOR as u8;}
+    for c in vec {
+        if *c == foreign_slash as u8 { *c = MAIN_SEPARATOR as u8;}
     }
 
     path
